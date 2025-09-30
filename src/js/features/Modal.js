bee6ef5bc0dfa16ded9301ae905fffb7767388bf
vitor-gblop <vitor.gblop@gmail.com>
2025-09-30 12:36:57 -0500

    [feat] abrir e fechar modais de salvamento

---
+++ src/js/features/Modal.js
@@ -0,0 +1,11 @@
+
+let closeModal = () => {
+    let modal = document.getElementById("modal-template");
+
+    modal.style.display = "none"
+}
+let openModal = () => {
+    let modal = document.getElementById("modal-template");
+
+    modal.style.display = "flex"
+}
