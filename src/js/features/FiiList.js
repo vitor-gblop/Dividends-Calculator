2f9e97131fc462de3aae4d41e4712da9701c07de
vitor-gblop <vitor.gblop@gmail.com>
2025-09-30 12:36:16 -0500

    ?

---
+++ src/js/features/FiiList.js
@@ -0,0 +1,58 @@
+
+function listItensSaved()
+{
+    const fiiContainer = document.getElementById("fii-list-container");
+    fiiContainer.innerHTML = ''; // Clear the list before adding new items
+    
+    let d = {...localStorage}
+    
+    for(const itemKey in d)
+    {
+        if (localStorage.hasOwnProperty(itemKey)) 
+        {
+            const itemData = JSON.parse(localStorage.getItem(itemKey));
+            const newElement = createElement(itemData);
+
+            fiiContainer.appendChild(newElement);
+        }
+    }
+}
+
+
+const createElement = (itemData) => {
+    const investmentDiv = document.createElement('div'); //  cria uma div fii-list-investiment // 
+
+    // define os atributos // 
+    investmentDiv.id = 'fii-list-investiment';
+    investmentDiv.className = 'display-flex flex-justify-between';
+
+    const siglaNameContainer = document.createElement('div');
+    siglaNameContainer.id = 'sigla-name-container';
+
+    const siglaSpan = document.createElement('span');
+    siglaSpan.id = 'fii-sigla';
+    siglaSpan.textContent = itemData.sigla; 
+
+    const nameSpan = document.createElement('span');
+    nameSpan.id = 'fii-name';
+    nameSpan.textContent = itemData.shortName;
+
+    siglaNameContainer.appendChild(siglaSpan);
+    siglaNameContainer.appendChild(document.createElement('br'));
+    siglaNameContainer.appendChild(nameSpan);
+
+    const valueSpan = document.createElement('span');
+    valueSpan.id = 'fii-value';
+    // valueSpan.textContent = `${itemData.lastCloseValue.toFixed(2)} R$`;
+    valueSpan.textContent = `${itemData.value} R$`;
+
+    const gainSpan = document.createElement('span');
+    gainSpan.id = 'fii-gain';
+    gainSpan.textContent = '0.0 R$'; // Placeholder for gain
+
+    investmentDiv.appendChild(siglaNameContainer);
+    investmentDiv.appendChild(valueSpan);
+    investmentDiv.appendChild(gainSpan);
+
+    return investmentDiv;
+}
