fe5f85827405eea760cda6d9cf5cafe32a51915a
vitor-gblop <vitor.gblop@gmail.com>
2025-10-01 20:37:08 +0000

    [feat] adicionando verificação de login

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,9 @@ var intervalId = setInterval(
     function() 
     {
         listItensSaved();
+        loginVerification();
     }, 3000
 );
 // You can clear a periodic function by uncommenting:
-clearInterval(intervalId);
+// clearInterval(intervalId);
+
